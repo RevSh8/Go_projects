3a9efcbc922d9b6cc4703ea5f556a6a061615fbb
DarjaShiri <pelemeshka6@yandex.ru>
2024-10-12 15:20:01 +0300

    Adding a ToFloat func

---
+++ toFloat.go
@@ -0,0 +1,22 @@
+package toFloat
+
+import (
+	"bufio"
+	"strings"
+	"os"
+	"strconv"
+)
+
+func ToFloat()(float, error){
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err!=nil{
+		return 0, err
+	}
+	input = strings.TrimSpace(input)
+	res, err := strconv.ParseFloat(input, 64)
+	if err != nil {
+		return 0, err
+	}
+	return res, nil
+}
